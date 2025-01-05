3695cca9a3cb5660b653c9eeabb88f5e2243f3c6
myko98 <michaelluchen1998@gmail.com>
2025-01-05 18:00:46 -0500

    delete backend stuff

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-class Config:
-    # 07 29 - I set the MONGO_URI, which points to a mongo cluster, so if you want to go back to localhost, just use localhost url vs MONGO_URI
-    MONGO_URI = os.getenv(
-        'MONGO_URI_FAKE', 'mongodb://localhost:27017/sakeDatabase')
-    # 09 22 - comment out mongo_uri to use local host
