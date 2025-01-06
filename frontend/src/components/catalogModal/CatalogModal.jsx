2613e08833f19ff346be3fb1d8c6da62c1bf011c
myko98 <michaelluchen1998@gmail.com>
2025-01-05 19:34:28 -0500

    Fix catalog modal

--- frontend/src/components/catalogModal/CatalogModal.jsx
+++ frontend/src/components/catalogModal/CatalogModal.jsx
@@ -5,7 +5,18 @@ const CatalogModal = ({ handleModalStatus, selectedCard }) => {
 	console.log(selectedCard);
 	const { name, image, description, price, properties } = selectedCard
 
-	
+	const PROPERTIES = [["alchoholPercentage", "Alchohol Percentage"]
+		, ["body", "Body"]
+		, ["brewery", "Brewery"]
+		, ["fermentationStyle", "Fermentation Style"]
+		, ["inStock", "In stock"]
+		, ["pairing", "Pairing"]
+		, ["polish", "Polish"]
+		, ["region", "Region"]
+		, ["riceType", "Rice Type"]
+		, ["sizes", "Sizes"]
+		, ["style", "Style"]
+		, ["taste", "Taste"]]
 
 	return (
 		<div className={styles.modalOverlay} onClick={() => handleModalStatus(false)}>
@@ -18,16 +29,16 @@ const CatalogModal = ({ handleModalStatus, selectedCard }) => {
 					<div className={styles.header}>
 						<h2>{name}</h2>
 						<h2>Price: {price}</h2>
+						<h2>Product Details</h2>
+						<p>{description}</p>
 					</div>
-					<p>{description}</p>
-					<h2>More Details</h2>
 					<div className={styles.properties}>
-						{/* {propertiesArr.map(([key, value]) =>
-							<div key={key} className={styles.gridRow}>
-								<p>{key}:</p>
-								<p style={{ textAlign: "right" }}>{value}</p>
-							</div>
-						)} */}
+						{PROPERTIES.map((pair) =>
+							<div key={pair[0]} className={styles.gridRow}>
+								<p>{pair[1]}:</p>
+								<p style={{ textAlign: "right" }}>{properties[pair[0]]}</p>
+							</div>)
+						}
 					</div>
 				</div>
 			</div>
